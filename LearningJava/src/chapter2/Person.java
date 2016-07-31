2f4b29c12b5a63deb1d5dd5a1dbcade3a408f1ef
shenaikabilova <shenito93@abv.bg>
2016-07-31 22:44:46 +0300

    class Person

---
+++ LearningJava/src/chapter2/Person.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class Person {
+	private String name;
+	private String EGN;
+	
+	public Person (String name, String EGN) {
+		this.name = name;
+		this.EGN = EGN;
+	}
+}
